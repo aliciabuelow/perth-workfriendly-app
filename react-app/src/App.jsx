419940f97740be040199c4057e315d4a319cbc91
Alicia Buelow <alicia@buelow.com.au>
2026-05-06 14:46:13 +0800

    Create EmptyState component and filter spots based upon search term

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,20 +3,32 @@ import './App.css';
 import { spots } from './data/spots.js';
 import SpotList from './components/SpotList';
 import SearchBar from './components/SearchBar';
+import EmptyState from './components/EmptyState';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
-    console.log(searchTerm);
   }
+
+  const search = searchTerm.toLowerCase();
+
+  const filteredSpots = spots.filter((spot) => {
+    return (
+    spot.name.toLowerCase().includes(search) ||
+    spot.suburb.toLowerCase().includes(search) ||
+    spot.type.toLowerCase().includes(search)
+  )
+  });
 
   return (
     <div className="App">
       <h1>Perth Work-Friendly</h1>
       <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
-      <SpotList spots={spots} />
+
+      {filteredSpots.length > 0 ? <SpotList spots={filteredSpots} /> : <EmptyState />}
+
     </div>
   )
 }
---
+++ react-app/src/components/EmptyState.jsx
@@ -0,0 +1,8 @@
+export default function EmptyState() {
+    return (
+        <div className="EmptyState">
+            <h2>No spots found.</h2>
+            <p>Try adjusting your search.</p>
+        </div>
+    )
+}
