3045b62ef6851605ff24b75741e904c116e8ceed
Alicia Buelow <alicia@buelow.com.au>
2026-05-07 19:53:37 +0800

    Style SearchBar, adjust page spacing

--- react-app/src/components/EmptyState.jsx
+++ react-app/src/components/EmptyState.jsx
@@ -1,8 +1,8 @@
 export default function EmptyState() {
     return (
         <div className="EmptyState">
-            <h2>No spots found.</h2>
-            <p>Try adjusting your search.</p>
+            <h2 className="empty-state">Sorry, no spots found!</h2>
+            <p className="empty-state">Try adjusting your search.</p>
         </div>
     )
 }
--- react-app/src/components/SearchBar.jsx
+++ react-app/src/components/SearchBar.jsx
@@ -1,8 +1,12 @@
+import '../styles/SearchBar.css';
+
 export default function SearchBar(props) {
     return (
         <div className="search-bar">
             <input 
-            type="text" 
+            type="search" 
+            aria-label="Search spots by name, suburb, or type"
+            className="search-input"
             placeholder="Search by name, suburb, or type..."
             value={props.searchTerm}
             onChange={props.handleSearch}
