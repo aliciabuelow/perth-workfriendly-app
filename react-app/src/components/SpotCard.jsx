cdbe205d9b6318bdc684e80af3c9daf1d01216c7
Alicia Buelow <alicia@buelow.com.au>
2026-05-07 14:30:29 +0800

    Create SpotCard structure and hierarchy

--- react-app/src/components/SpotCard.jsx
+++ react-app/src/components/SpotCard.jsx
@@ -1,13 +1,35 @@
 export default function SpotCard({ spot }) {
     return (
-        <div className="spot-card">
+        <article className="spot-card">
+            <header className="card-heading">
+                <div className="type-badge">{spot.type}</div>
+                <h2 className="card-name">{spot.name}</h2>
+                <div className="suburb">{spot.suburb}</div>
+            </header>
 
-            <h2>{spot.name}</h2>
-            <p>{spot.suburb}</p>
-            <p>{spot.type}</p>
-            <p>{spot.address}</p>
-            <p>{spot.notes}</p>
+            <div className="card-body">
+                <p className="notes">{spot.notes}</p>
 
-        </div>
+                <div className="spot-tags">
+                    {spot.tags.map((item) => (
+                        <div key={item} className="spot-tag">
+                            {item}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <footer className="card-footer">
+                <div className="address">{spot.address}</div>
+                <a 
+                 href={spot.website} 
+                 target="_blank" 
+                 rel="noopener noreferrer" 
+                 className="website-link"
+                 >
+                    Visit Website
+                </a>
+            </footer>
+        </article>
     )
 }
